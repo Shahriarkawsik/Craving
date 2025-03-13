d2d5c5df00e999d51e801599a62396f0783e19bb
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-03-13 22:16:15 +0600

    featured food done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Banner from "@/components/homeComponents/banner";
+import Category from "@/components/homeComponents/Category";
+import FeaturedFood from "@/components/homeComponents/FeaturedFood";
 
 export default function Home() {
   return (
-    <section className="lg:space-y-10 lg:my-10">
+    <section className="lg:space-y-10 lg:my-10 bg-amber-100">
       {/* Banner section */}
       <Banner />
-      {/* Banner section */}
+      {/* Category Section */}
+      <Category />
+      {/* Featured Food */}
+      <FeaturedFood />
       <h1 className="text-center mt-50 text-2xl font-bold mb-5">
         This is Food delivery project
       </h1>
---
+++ src/components/homeComponents/Category.tsx
@@ -0,0 +1,17 @@
+const Category = () => {
+  return (
+    <section className="w-11/12 mx-auto space-x-5 text-center">
+      <button className=" rounded-xl p-3 bg-amber-200 text-black font-bold text-2xl">
+        Fast Food
+      </button>
+      <button className=" rounded-xl p-3 bg-amber-200 text-black font-bold text-2xl">
+        Drinks
+      </button>
+      <button className=" rounded-xl p-3 bg-amber-200 text-black font-bold text-2xl">
+        Dessert
+      </button>
+    </section>
+  );
+};
+
+export default Category;
