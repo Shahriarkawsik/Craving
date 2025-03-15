eeabb75fae317bb9188547f55af1de2a93669ab6
TurjoSiam <t.siam24@gmail.com>
2025-03-16 05:24:41 +0600

    [ADDED]-title and subtitle in contact us banner image added

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -15,7 +15,14 @@ const ContactUs = () => {
                     width: "100%",
                 }}
             >
-
+                <div className="w-3xl mx-auto text-center z-50">
+                    <h2 className="text-2xl md:text-4xl font-bold text-white">
+                        Contact Us
+                    </h2>
+                    <p className="mt-2 text-gray-300">
+                    Get in touch with us effortlessly! Whether you have questions, feedback, or need assistance, we are here to help. Reach out and let’s connect!
+                    </p>
+                </div>
             </div>
         </div>
     );
