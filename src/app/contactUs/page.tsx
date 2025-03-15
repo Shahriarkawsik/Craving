936e94d3f87523c31d8f3db56d10fd9cf7c5493a
TurjoSiam <t.siam24@gmail.com>
2025-03-16 05:18:21 +0600

    [ADDED]-banner image in contact us page added

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -1,9 +1,22 @@
-
+import contactUsBanner from "@/assets/bannerImg/contactUs-banner.jpg"
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1 className="text-center mt-50 text-2xl font-bold mb-5">This is Contact page</h1>
+        <div className="bg-gray-100 min-h-screen">
+            {/* Banner section */}
+            <div
+                className="flex justify-center items-center"
+                style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${contactUsBanner.src})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    height: "280px",
+                    width: "100%",
+                }}
+            >
+
+            </div>
         </div>
     );
 };
