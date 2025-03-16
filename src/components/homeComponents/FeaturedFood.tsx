ca1aad4e22f88fcd73f6b9b1bb1a8998d6e26d52
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-03-16 23:52:16 +0600

    fix error

--- src/components/homeComponents/FeaturedFood.tsx
+++ src/components/homeComponents/FeaturedFood.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 import foodImg from "../../assets/bannerImg/dish-banner-003.jpg";
 
 const FeaturedFood = () => {
-  interface featuredFood {
-    name: string;
-    description: string;
-    price: number;
-    rating: number;
-  }
-  const featuredFoods: featuredFood[] = [
-    {
-      name: "Barger",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, perspiciatis.",
-      price: 100, // Replace with the actual price
-      rating: 5, // Replace with the actual rating
-    },
-  ];
+  // interface featuredFood {
+  //   name: string;
+  //   description: string;
+  //   price: number;
+  //   rating: number;
+  // }
+  // const featuredFoods: featuredFood[] = [
+  //   {
+  //     name: "Barger",
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, perspiciatis.",
+  //     price: 100, // Replace with the actual price
+  //     rating: 5, // Replace with the actual rating
+  //   },
+  // ];
   return (
     <section className="w-11/12 mx-auto text-center space-y-5">
       <h1 className="text-5xl font-bold text-orange-600 uppercase border-b-5 border-t-5 border-orange-300 p-3 inline-block">
