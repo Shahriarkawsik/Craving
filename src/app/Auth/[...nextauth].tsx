6da55eac40688963d4c6c0549faf1b5536819718
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-03-18 15:01:11 +0600

    [ADDED]: added route before adding database

--- src/app/Auth/[...nextauth].tsx
+++ src/app/Auth/[...nextauth].tsx
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -9,8 +9,7 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     // ...add more providers here
-    
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
