846ff60e69c863bc46a281c0e99db1d19107428a
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-20 13:29:21 +0600

    [UPDATED]: updated register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,12 +4,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { registerUser } from "../action/auth/allApi";
 import { FormEvent } from "react";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 // import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Register = () => {
-  const router = useRouter()
+  const router = useRouter();
   // const [error, setError] = useState<string | null>(null)
 
   // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
@@ -22,26 +23,26 @@ const Register = () => {
     const password = (form.elements.namedItem("password") as HTMLInputElement)
       .value;
 
-      // if (!passwordRegex.test(password)) {
-      //   toast.error("Register failed");
-      //   setError("Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number.");
-      //   return;
-      // }
+    // if (!passwordRegex.test(password)) {
+    //   toast.error("Register failed");
+    //   setError("Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number.");
+    //   return;
+    // }
 
     try {
-      await registerUser({name, email, password});
+      await registerUser({ name, email, password });
       toast.success("User register is successfully", {
         position: "top-center",
-        autoClose:1000,
-      })
-      form.reset()
-      router.push('/')
+        autoClose: 1000,
+      });
+      form.reset();
+      router.push("/");
     } catch (error) {
-      console.log(error)
-      toast.error("User register is failed", {
+      console.log(error);
+      toast.error("Already have an account", {
         position: "top-center",
-        autoClose:1000,
-      })
+        autoClose: 1000,
+      });
     }
   };
 
@@ -86,14 +87,61 @@ const Register = () => {
           </div>
 
           <div className="mt-6 text-center">
-            <Button variant="outline" type="submit">
+            <Button
+              variant="outline"
+              type="submit"
+              className="w-full p-3 text-white bg-green-600 rounded-lg hover:bg-green-700 hover::text-white transition duration-300"
+            >
               Sign Up
             </Button>
           </div>
         </form>
       </div>
+      <div className="relative flex items-center justify-center my-4">
+        <div className="w-full h-px bg-gray-300"></div>
+        <span className="absolute bg-white px-4 text-gray-500">or</span>
+      </div>
+      <p className="text-center text-gray-600 text-sm">
+        Already have an account?{" "}
+        <Link href="/signIn" className="text-green-600 hover:underline">
+          Sign In
+        </Link>
+      </p>
     </div>
   );
 };
 
 export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try {
+//   const result: { insertedId?: string } | 'Already Have an Account' | null = await registerUser(userData);
+
+//   if (result && typeof result === 'object' && 'insertedId' in result) {
+//       await signIn('credentials', { email, password, redirect: false });
+//       router.push('/')
+//       toast.success('Account Created Successfully!');
+//       console.log(result);
+//   } else if (result === 'Already Have an Account') {
+//       toast.error('Already Registered');
+//   } else {
+//       toast.error('Registration failed. Please try again later.');
+//   }
+// } catch (error) {
+//   console.error(error);
+// }
+// };
