9a2fb0291d9d1530b5eb3eed8c73686f62b56aee
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-21 03:44:01 +0600

    [ADDED]: added some design in dash board

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -15,77 +15,82 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          {/* all admin dashboard */}
-          <SidebarGroupLabel><small className="text-xl font-semibold bg-green-500 text-white px-3 rounded-xl">{role}</small></SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
+          <SidebarGroupLabel>
+            <small className="text-xl font-semibold bg-green-500 text-white px-3 rounded-xl mb-2">
+              {role}
+            </small>
+          </SidebarGroupLabel>
+            {/* all admin dashboard */}
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <Link href="/dashboard/admin/allResturant">
+                    All Resturants
+                  </Link>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <Link href="/dashboard/admin/allRiders">All Riders</Link>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <Link href="/dashboard/admin/statistics">statistics</Link>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <Link href="/dashboard/admin/applications">Applications</Link>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <Link href="/dashboard/admin/reviewApplication">
+                    Review Applications
+                  </Link>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+            {/* resturant owner's dashboard */}
+            <SidebarGroupContent>
               <SidebarMenuItem>
-                <Link href="/dashboard/admin/allResturant">All Resturants</Link>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <Link href="/dashboard/admin/allRiders">All Riders</Link>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <Link href="/dashboard/admin/statistics">statistics</Link>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <Link href="/dashboard/admin/applications">Applications</Link>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <Link href="/dashboard/admin/reviewApplication">
-                  Review Applications
+                <Link href="/dashboard/resturantOwner/addFoodItem">
+                  Add Food Item
                 </Link>
               </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-          {/* resturant owner's dashboard  */}
-          <SidebarGroupContent>
-            <SidebarMenuItem>
-              <Link href="/dashboard/resturantOwner/addFoodItem">
-                Add Food Item
-              </Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/resturantOwner/allFoodItem">
-                All Food Item
-              </Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/resturantOwner/addResturant">
-                Add Resturant
-              </Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/resturantOwner/statistics">
-                Statistics
-              </Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/resturantOwner/orderHistory">
-                Order History
-              </Link>
-            </SidebarMenuItem>
-          </SidebarGroupContent>
-          {/* riders dashboard  */}
-          <SidebarGroupContent>
-             <SidebarMenuItem>
-              <Link href="/dashboard/riders/availableOrders">
-                Available Orders
-              </Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/riders/myOrders">My Orders</Link>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Link href="/dashboard/riders/orderHistory">Orders History</Link>
-            </SidebarMenuItem>
-          </SidebarGroupContent>
+              <SidebarMenuItem>
+                <Link href="/dashboard/resturantOwner/allFoodItem">
+                  All Food Item
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link href="/dashboard/resturantOwner/addResturant">
+                  Add Resturant
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link href="/dashboard/resturantOwner/statistics">
+                  Statistics
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link href="/dashboard/resturantOwner/orderHistory">
+                  Order History
+                </Link>
+              </SidebarMenuItem>
+            </SidebarGroupContent>
+            {/* riders dashboard */}
+            <SidebarGroupContent>
+              <SidebarMenuItem>
+                <Link href="/dashboard/riders/availableOrders">
+                  Available Orders
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link href="/dashboard/riders/myOrders">My Orders</Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link href="/dashboard/riders/orderHistory">
+                  Orders History
+                </Link>
+              </SidebarMenuItem>
+            </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
 }
 export default AppSidebar;
-
-
- 
