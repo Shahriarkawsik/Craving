22adba17230ca35449f1069e0dfdd6d29842d9fc
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-23 08:58:27 +0600

    [ADDED]:register page redirect functionality added is done

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -9,7 +9,6 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { usePathname } from "next/navigation";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -21,11 +20,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Navbar = () => {
   const pathName = usePathname();
-  const { data: session, status } = useSession();
-  console.log(session);
+  // const { data: session, status } = useSession();
+  const { data, status } = useSession();
+  console.log(data, "all data")
+  console.log(data?.user?.image, "=====================================");
   return (
     <header className=" shadow-md py-4">
       <nav className="flex justify-between items-center w-11/12 mx-auto px-4 md:px-8">
@@ -85,12 +87,14 @@ const Navbar = () => {
           </NavigationMenu>
         </div>
         <div className="flex items-center gap-3">
-          <div>
-            <Avatar>
-              {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
+          {data && (
+            <div>
+              <Avatar>
+                <AvatarImage src={data?.user?.image as string | undefined} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          )}
           <div className="hidden md:flex">
             {status == "authenticated" ? (
               <Button variant="destructive" onClick={() => signOut()}>
