8bb845f918c2d7f5da6d8ca546a8fd52e51118ba
mahmudrkb <almahmudrkb98@gmail.com>
2025-03-25 09:42:25 +0600

    [added]: added dashboard navbar

--- src/app/(dashboardLayout)/dashboard/layout.tsx
+++ src/app/(dashboardLayout)/dashboard/layout.tsx
@@ -1,14 +1,20 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./page";
 
+import DashNavbar from "./dashNavbar/page";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
+    <SidebarProvider className="">
       <AppSidebar />
-      <main className="flex-1">
-        <SidebarTrigger />
-        {children}
-      </main>
+      <SidebarTrigger />
+      <div className="flex w-11/12 flex-col mx-5   gap-4">
+        <nav>
+          {" "}
+          <DashNavbar />
+        </nav>
+        <main className="  ">{children}</main>
+      </div>
     </SidebarProvider>
   );
 }
