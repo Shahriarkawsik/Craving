a5e3dc47798c2a61f7489f6bcb7a82e98854c2a4
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-26 23:23:38 +0600

    [ADDED]: added nextAuthSessionProvider

---
+++ src/Providers/NextAuthSessionProvider.tsx
@@ -0,0 +1,25 @@
+
+// "use client"
+// // import { SessionProvider } from "next-auth/react";
+// import { ReactNode } from "react";
+// interface Props {
+//     children:ReactNode;
+// }
+// const NextAuthSessionProvider: React.FC<Props> = ({children}) => {
+//     return <SessionProvider>{children}</SessionProvider>
+// };
+
+// export default NextAuthSessionProvider;
+
+
+// =======================================
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+interface NextAuthSessionProviderProps {
+  children: ReactNode;
+}
+
+export default function NextAuthSessionProvider({ children }: NextAuthSessionProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
