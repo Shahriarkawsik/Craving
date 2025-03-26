8a8ef35384e27a39a65573ea11cbc7bdf4eba5b5
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-27 00:13:39 +0600

    [ADDED]:added userInfo file

--- src/Providers/NextAuthSessionProvider.tsx
+++ src/Providers/NextAuthSessionProvider.tsx
@@ -1,25 +1,26 @@
 
-// "use client"
-// // import { SessionProvider } from "next-auth/react";
-// import { ReactNode } from "react";
-// interface Props {
-//     children:ReactNode;
-// }
-// const NextAuthSessionProvider: React.FC<Props> = ({children}) => {
-//     return <SessionProvider>{children}</SessionProvider>
-// };
+"use client"
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+interface Props {
+    children:ReactNode;
+}
+const NextAuthSessionProvider: React.FC<Props> = ({children}) => {
+    return <SessionProvider>{children}</SessionProvider>
+};
 
-// export default NextAuthSessionProvider;
+export default NextAuthSessionProvider;
 
 
 // =======================================
-import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
+// "use-client"
+// import { ReactNode } from "react";
+// import { SessionProvider } from "next-auth/react";
 
-interface NextAuthSessionProviderProps {
-  children: ReactNode;
-}
+// interface NextAuthSessionProviderProps {
+//   children: ReactNode;
+// }
 
-export default function NextAuthSessionProvider({ children }: NextAuthSessionProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
+// export default function NextAuthSessionProvider({ children }: NextAuthSessionProviderProps) {
+//   return <SessionProvider>{children}</SessionProvider>;
+// }
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -64,29 +64,30 @@
 
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials"
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        Email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        console.log(req)
-        console.log("credentials from auth", credentials)
-        const user = {id: "1", name:"shahidul", email:"shahidul@gmail.com"}
-        console.log(user)
+const authOptions = {
+providers: [
+  CredentialsProvider({
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      Email: { label: "Email", type: "email" },
+      password: { label: "Password", type: "password" },
+    },
+    async authorize(credentials, req) {
+      console.log(req)
+      console.log("credentials from auth", credentials)
+      const user = {id: "1", name:"shahidul", email:"shahidul@gmail.com"}
+      console.log(user)
 
-        // If no error and we have user data, return it
-        if (user) {
-          return user;
-        }
-        // Return null if user data could not be retrieved
-        return null;
-      },
-    }),
-  ],
-});
+      // If no error and we have user data, return it
+      if (user) {
+        return user;
+      }
+      // Return null if user data could not be retrieved
+      return null;
+    },
+  }),
+],
+}
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
