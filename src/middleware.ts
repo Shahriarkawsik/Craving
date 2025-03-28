c5b5d978ae2cd0e378e7f42562af637c231d499b
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-03-28 22:22:20 +0600

    [UPDATE]: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,13 @@ import { NextResponse } from "next/server"
 
 export const middleware = async (req) => {
     const token = await getToken({req})
-    if(token) console.log("from middleware token", token)
+    // if(token) console.log("from middleware token", token)
+    const isTokenOk = Boolean(token)
+    const isAdminUser = token?.role === "User"
+    const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboard")
+    if(isAdminSpecificRoute && !isAdminUser){
+        const callbackUrl = encodeURIComponent(req.nextUrl.pathname)
+        return NextResponse.redirect(new URL(`/signIn?callbackUrl=${callbackUrl}`, req.url))
+    }
     return NextResponse.next();
 }
