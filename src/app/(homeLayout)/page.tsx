9720719d9e6f254b10719cd9c7b15d2ddf4811b1
mahmudrkb <almahmudrkb98@gmail.com>
2025-04-05 18:20:58 +0600

    [update]: countdown bg update

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -15,8 +15,10 @@ export default function Home() {
     <section className="lg:space-y-20">
       {/* bg-amber-100 */}
       {/* Banner section */}
+      <div>
       <Banner />
-         <CountdownTimer targetDate="2025-04-10T00:00:00" />
+      <CountdownTimer targetDate="2025-04-10T00:00:00" />
+      </div>
       <EidCart/>
    
 
--- src/components/homeComponents/CountdownTimer.tsx
+++ src/components/homeComponents/CountdownTimer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import eid from "@/assets/eid3.jpg";
 
 interface CountdownTimerProps {
   targetDate: string; // e.g. "2025-04-10T00:00:00"
@@ -31,16 +32,34 @@ const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
   }, []);
 
   return (
-    <div className="w-11/12 mx-auto ">
-      <div className="flex justify-center gap-4 text-center  font-mono">
-        {["days", "hours", "minutes", "seconds"].map((unit) => (
-          <div key={unit}>
-            <div className="text-5xl font-bold text-blue-600">
-              {(timeLeft as any)[unit].toString().padStart(2, "0")}
+    <div className="  mx-auto  space-y-4">
+      <div
+        className="flex-col flex space-y-4 justify-center items-center"
+        style={{
+          backgroundImage: `url(${eid.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "90vh ",
+          width: "100%",
+        }}
+      >
+        <h2 className="font-mono text-2xl text-[#c27f27] font-bold ">
+          {" "}
+          EID IS IN
+        </h2>
+
+        <div className="flex justify-center items-center gap-4 text-center  font-mono">
+          {["days", "hours", "minutes", "seconds"].map((unit) => (
+            <div key={unit}>
+              <div className="text-5xl font-bold text-[#c27f27] ">
+                {(timeLeft as any)[unit].toString().padStart(2, "0")}
+              </div>
+
+              <div className="text-sm capitalize">{unit}</div>
             </div>
-            <div className="text-sm capitalize">{unit}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
