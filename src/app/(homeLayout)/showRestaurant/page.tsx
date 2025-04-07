ba4911fccd2cfc7f5e6b6751789000e08ea35138
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-04-07 15:41:24 +0600

    [UPDATE]: updated some file extension and add some file for deploy

--- src/app/(homeLayout)/showRestaurant/page.tsx
+++ src/app/(homeLayout)/showRestaurant/page.tsx
@@ -10,35 +10,6 @@ interface ResturantType {
   email: string;
 }
 
-<<<<<<< HEAD:src/app/(homeLayout)/showRestaurant/page.tsx
-const ShowRestaurant = async () => {
-    // Get the database instance
-    const db = await dbConnect();
-
-    // Get the collection
-    const restaurantCollection: Collection<ResturantType> = db.collection("resturant");
-
-    // Fetch data from MongoDB
-    const data: ResturantType[] = await restaurantCollection.find({}).toArray();
-
-    return (
-        <div>
-            <h1 className="text-center">Resturant</h1>
-            <ul>
-                {data.map((restaurant) => (
-                    <div key={restaurant._id}>
-                        <h1 className="text-center">{restaurant.title}</h1>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-export default ShowRestaurant;
-
-
-=======
 const Services = async () => {
   // Get the database instance
   const db = await dbConnect();
@@ -65,4 +36,3 @@ const Services = async () => {
 };
 
 export default Services;
->>>>>>> 3a551569826a054f1c0936e5fea2c6e3ae27f881:src/app/(homeLayout)/showResturant/page.tsx
