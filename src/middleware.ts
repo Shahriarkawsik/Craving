b2465a714ebd5d61ddad62a002697f623ce8e843
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-04-09 01:18:06 +0600

    [UPDATED]: update middleware page

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export const middleware = async (req: NextRequest) => {
     const token = await getToken({req})
     // if(token) console.log("from middleware token", token)
     // const isTokenOk = Boolean(token)
-    const isAdminUser = token?.role === "Admin" || token?.role === "Rider" || token?.role === "RestaurantOwner"  
+    const isAdminUser = token?.role === "Admin" || token?.role === "RestaurantOwner" || token?.role === "Rider"
     const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/dashboard")
     if(isAdminSpecificRoute && !isAdminUser){
         const callbackUrl = encodeURIComponent(req.nextUrl.pathname)
