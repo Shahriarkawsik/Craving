92210963578bc6017656d99946c5002252d12109
mahmudrkb <almahmudrkb98@gmail.com>
2025-04-16 23:26:30 +0600

    [update] eidcard and weservice page   update done

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
       {/* Banner section */}
       <div>
         <Banner />
-        <CountdownTimer targetDate="2025-06-12T00:00:00" />
+        <CountdownTimer targetDate="2025-06-10T00:00:00" />
       </div>
       <EidCart />
 
