2983aabddcaa02a4a2143e2c899e769944f40cb2
Jakaria030 <gulamjakaria99@gmail.com>
2025-04-17 22:03:19 +0600

    Single food details page completed

--- src/components/homeComponents/FeaturedFood.tsx
+++ src/components/homeComponents/FeaturedFood.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { FoodDetails, getFeaturedFood } from "@/app/action/auth/allApi";
 import FoodCard from "../shared/FoodCard";
+import { useRouter } from "next/navigation";
 
 const FeaturedFood = () => {
+  const router = useRouter();
 
   const [featuredFoods, setFeaturedFoods] = useState<FoodDetails[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -45,10 +47,14 @@ const FeaturedFood = () => {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-10">
           {featuredFoods.map((food) => (
-            <FoodCard key={food.food_id} food={food}/>
+            <FoodCard key={food.food_id} food={food} />
           ))}
         </div>
       )}
+
+      <div className="text-black w-fit mx-auto mt-10">
+        <button onClick={() => router.push('/allFood')} className="px-4 py-2 border-2 border-orange-500 text-black hover:text-white hover:bg-orange-500 transition-colors duration-200 rounded-sm cursor-pointer">See All Food</button>
+      </div>
     </section>
   );
 };
